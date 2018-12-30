5dc71a73f734136ae8e8e722812848bf734a4b5f
Ameya-Joshi79 <amey523@gmail.com>
2018-12-30 15:47:35 -0500

    Created a landscape layout for the Dice App i.e. the layout that will be displayed when the App is in the Landscape mode. 1. The Device/Android system uses the activitymainstartingpoint.xml layout xml file in the layout folder for when the Device is in the Portrait mode. 2. The Device/Android system uses the activitymainstartingpoint.xml layout xml file in the layout-land folder for when the Device is in the Landscape mode Note: i. We have added 2 Horizontal guidelines to this files and made the Las Vegas ImageView's height equal to match_constraint so that it will always be within these constraints and not overlap with the Dice images ii. We made the vertical constraints a little so that the Las Vegas ImageView's left and right constraints are connected to these vertical guidelines. We also set the layout_width attribute of the ImageView to wrap_content

---
+++ app/src/main/java/com/londonappbrewery/dice_nested_debug/MainActivityNew.java
@@ -0,0 +1,13 @@
+package com.londonappbrewery.dice_nested_debug;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivityNew extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activitymainstartingpoint);
+    }
+}
